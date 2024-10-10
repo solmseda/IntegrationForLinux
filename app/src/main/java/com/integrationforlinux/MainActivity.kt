7a99fabd14373efb09b5dd3e983a818e0d635b4f
Sol <solmseda@gmail.com>
2024-10-10 14:07:40 -0300

    Limpeza no código

--- app/src/main/java/com/integrationforlinux/MainActivity.kt
+++ app/src/main/java/com/integrationforlinux/MainActivity.kt
@@ -7,16 +7,11 @@ import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.content.IntentFilter
-import android.content.pm.PackageManager
-import android.os.Build
 import android.os.Bundle
-import android.util.Log
 import android.widget.ArrayAdapter
 import android.widget.ListView
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.app.ActivityCompat
-import androidx.core.content.ContextCompat
 
 class MainActivity : AppCompatActivity() {
     private lateinit var statusTextView: TextView
